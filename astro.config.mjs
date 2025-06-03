3d26626d0b068cc5351eb415ac327cb9d40708e6
LongYC <contact@longyc.com>
2025-06-03 18:57:07 +0800

    Add base to config instead of using conditional favicon path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/masa',
   env: {
     schema: {
-      FAVICON_PATH: envField.string({ context: "client", access: "public" })
+      FAVICON_PATH: envField.string({ context: 'client', access: 'public' })
     }
   }
 });
