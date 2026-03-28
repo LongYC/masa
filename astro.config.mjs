1115ce543809de615633ea3fe339029affa19644
LongYC <contact@longyc.com>
2026-03-28 23:35:22 +0800

    Switch to Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   base: '/masa',
+
   env: {
     schema: {
       FAVICON_PATH: envField.string({ context: 'client', access: 'public' })
     }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
